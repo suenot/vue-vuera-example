52c5cb539a3e2b755627c3c8e8b409e91b1fae9e
Eugen Soloviov <suenot@gmail.com>
2018-11-12 20:03:38 +0300

    start: react component doesn't work

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1></h1>
+    <MyVueComponent msg="I'm a Vue component"/>
+    <my-react-component />
+    <!-- <my-react-component :message="message" @reset="reset" /> -->
+  </div>
+</template>
+
+<script>
+import MyVueComponent from './components/MyVueComponent.vue'
+import MyReactComponent from './components/MyReactComponent'
+
+export default {
+  name: 'app',
+  components: {
+    MyVueComponent,
+    'my-react-component': MyReactComponent
+  }
+}
+</script>
---
+++ src/components/MyReactComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Empty extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>I'm a React component</h1>
+      </div>
+    )
+  }
+}
+
+export default Empty
